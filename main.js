22c4420ec23e6293df788f6e6ce1f47fc4ee1c90
OscarGlo <oscarglo@free.fr>
2018-11-21 14:47:32 +0100

    simple quotes server

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
-	res.writeHead(200, {"Content-Type": "text/html"});
-	fs.createReadStream("web/index.html").pipe(res);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.createReadStream('web/index.html').pipe(res);
 }).listen(80);
