153a86e84aa603e22a083b8660b68e91789d1254
OscarGlo <oscarglo@free.fr>
2018-11-21 23:15:38 +0100

    external vars

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,6 @@
-let to, sub_to, qu, sub_qu, sub_sub_qu, answ, prev, next;
+let to, sub_to, qu, sub_qu, sub_sub_qu, answ, prev, next,
+    id_to, id_qu, id_sub_qu,
+    topics, data;
 
 let nl2br = (str) => str.replace("\n", "<br>");
 
@@ -7,6 +9,36 @@ let elem = (tag) => document.querySelector(tag);
 function type(q_data, t) {
     if (q_data.t == null) return (t === "s");
     return q_data.t.includes(t);
+}
+
+function loadQuestion() {
+    let topic = topics[id_to],
+        q_data = data[topic][id_qu],
+        pipe = topic.indexOf("|");
+    
+    
+    to.innerHTML = topic.substr(0, pipe);
+    sub_to.innerHTML = (pipe !== -1 ? topic.substr(pipe + 1) : "");
+    
+    if (!type("p")) {
+        qu.innerHTML = q_data.n;
+        sub_qu.innerHTML = (q_data.s != null ? q_data.s : "");
+        
+        if (type(q_data, "g")) {
+            q_data = q_data.q[id_sub_qu];
+            sub_sub_qu.innerHTML = q_data.n;
+        } else
+            sub_sub_qu.innerHTML = "";
+        
+        // Generate input
+        if (type(q_data, "s") || type(q_data, "m")) {
+            let type = (type("s") ? "radio" : "checkbox");
+            answ.innerHTML = "";
+            for (let i = 0, len = data.a.length; i < len; ++i)
+                answ.innerHTML += `<input type="${type}" name="r" id="${i}" value="${name}"><label for="${i}">${name}</label><br>`;
+        } else if (type("t"))
+            answ.innerHTML = `<input type="text">`;
+    }
 }
 
 window.addEventListener("load", async () => {
@@ -19,41 +51,12 @@ window.addEventListener("load", async () => {
     prev = elem("#prev");
     next = elem("#next");
     
-    let data = await fetch("/json/questions.json").then(res => res.json()),
-        topics = Object.keys(data),
-        id_to = 0,
-        id_qu = 0,
-        id_sub_qu = 0;
+    data = await fetch("/json/questions.json").then(res => res.json());
     
-    function loadQuestion() {
-        let topic = topics[id_to],
-            q_data = data[topic][id_qu],
-            pipe = topic.indexOf("|");
-        
-        
-        to.innerHTML = topic.substr(0, pipe);
-        sub_to.innerHTML = (pipe !== -1 ? topic.substr(pipe + 1) : "");
-        
-        if (!type("p")) {
-            qu.innerHTML = q_data.n;
-            sub_qu.innerHTML = (q_data.s != null ? q_data.s : "");
-    
-            if (type(q_data, "g")) {
-                q_data = q_data.q[id_sub_qu];
-                sub_sub_qu.innerHTML = q_data.n;
-            } else
-                sub_sub_qu.innerHTML = "";
-    
-            // Generate input
-            if (type(q_data, "s") || type(q_data, "m")) {
-                let type = (type("s") ? "radio" : "checkbox");
-                answ.innerHTML = "";
-                for (let i = 0, len = data.a.length; i < len; ++i)
-                    answ.innerHTML += `<input type="${type}" name="r" id="${i}" value="${name}"><label for="${i}">${name}</label><br>`;
-            } else if (type("t"))
-                answ.innerHTML = `<input type="text">`;
-        }
-    }
+    topics = Object.keys(data);
+    id_to = 0;
+    id_qu = 0;
+    id_sub_qu = 0;
     
     loadQuestion();
 });
