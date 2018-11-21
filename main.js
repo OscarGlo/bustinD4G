af257b00c623f3b873672d8523fb36fc914ae4d7
OscarGlo <oscarglo@free.fr>
2018-11-21 17:52:58 +0100

    redirect /web/

--- main.js
+++ main.js
@@ -2,21 +2,10 @@ const server = require('http').createServer;
 const fs = require('fs');
 
 server(function(req, res) {
-    if (req.url === "/") req.url = "web/index.html";
-    try {
-        console.log(req.url);
-        if (fs.existsSync(req.url)) {
-            console.log("loaded " + req.url);
-            fs.createReadStream(req.url).pipe(res);
-        } else if (fs.existsSync(`web${req.url}`)) {
-            console.log("loaded web" + req.url);
-            fs.createReadStream(`web${req.url}`).pipe(res);
-        } else {
-            console.log("loaded default");
-            fs.createReadStream(`json/questions.json`).pipe(res);
-        }
-    } catch (e) {
-        console.log(e);
-        fs.createReadStream(`web/index.html`).pipe(res);
-    }
+    if (req.url === "/")
+        req.url = "web/index.html";
+    if (fs.existsSync(`web${req.url}`))
+        fs.createReadStream(`web${req.url}`).pipe(res);
+    else
+        fs.createReadStream("web/index.html").pipe(res);
 }).listen(80);
