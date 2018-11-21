bed1b7ff235230627551728c3d0098a5165dc6f6
OscarGlo <oscarglo@free.fr>
2018-11-21 17:29:52 +0100

    server redirect

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ const fs = require('fs');
 server(function(req, res) {
     if (req.url === "/") req.url = "web/index.html";
     try {
-        if (fs.existsSync(req.url))
+        console.log(req.url);
+        if (fs.existsSync(req.url)) {
+            console.log("loaded " + req.url);
             fs.createReadStream(req.url).pipe(res);
-        else if (fs.existsSync(`web${req.url}`))
+        } else if (fs.existsSync(`web${req.url}`)) {
+            console.log("loaded web" + req.url);
             fs.createReadStream(`web${req.url}`).pipe(res);
-        else
+        } else {
+            console.log("loaded default");
             fs.createReadStream(`json/questions.json`).pipe(res);
+        }
     } catch (e) {
         console.log(e);
         fs.createReadStream(`web/index.html`).pipe(res);
