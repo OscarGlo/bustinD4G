08d015f555efd9d2d1ddcf2b53829101eb49f1fb
OscarGlo <oscarglo@free.fr>
2018-11-21 14:31:21 +0100

    basic server

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const http = require("http");
+const fs = require("fs");
 
-http.createServer((req, res) => {
-	res.writeFile("web/index.html");
-	res.end();
+http.createServer(async (req, res) => {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.createReadStream('index.html').pipe(res);
 }).listen(80);
