98d7e57f6aaf2882c21bf9d14c5f0dc7945e3516
OscarGlo <oscarglo@free.fr>
2018-11-21 17:23:51 +0100

    server redirect

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 server(function(req, res) {
     if (req.url === "/") req.url = "web/index.html";
     try {
-        if (fs.existsSync(`${req.url}`))
-            fs.createReadStream(`${req.url}`).pipe(res);
+        if (fs.existsSync(req.url))
+            fs.createReadStream(req.url).pipe(res);
         else if (fs.existsSync(`web${req.url}`))
             fs.createReadStream(`web${req.url}`).pipe(res);
         else
             fs.createReadStream(`web/index.html`).pipe(res);
     } catch (e) {
+        console.log(e);
         fs.createReadStream(`web/index.html`).pipe(res);
     }
 }).listen(80);
