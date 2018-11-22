f853ccffe08fa6a10830b792f0255da5ee180c98
OscarGlo <oscarglo@free.fr>
2018-11-22 15:16:09 +0100

    jump log

--- web/js/script.js
+++ web/js/script.js
@@ -97,7 +97,7 @@ function next_qu() {
     
         let jump = (data[topics[id_to]].q ? data[topics[id_to]].q[id_qu].j : null);
         
-        console.log(data[topics[id_to]].q[id_qu], jump);
+        console.log((data[topics[id_to]].q ? data[topics[id_to]].q[id_qu] : "no q"), jump);
         
         if (jump) {
             [id_to, id_qu] = jump;
