da44879a76b8fd395f194408f9931cd6d8cd1e3b
OscarGlo <oscarglo@free.fr>
2018-11-22 13:22:37 +0100

    getanswer wip

--- web/js/script.js
+++ web/js/script.js
@@ -58,7 +58,7 @@ function loadQuestion() {
             for (let i = 0, len = ans.length; i < len; ++i) {
                 let name = ans[i],
                     nameText = name.replace("_", "<input type='text'>");
-                answ.innerHTML += `<label class="container">${nameText}<input type="${ty}" value="${name}" name="r">`
+                answ.innerHTML += `<label class="container"><input type="${ty}" name="r">${nameText}`
                     + `<span class="checkmark"></span></label>`;
             }
         } else if (type(q_data, "t"))
