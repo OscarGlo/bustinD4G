ae19e513b7c1c1d81026054fd1d95522456c5614
OscarGlo <oscarglo@free.fr>
2018-11-23 10:06:17 +0100

    parse ok

--- web/js/script.js
+++ web/js/script.js
@@ -375,8 +375,7 @@ window.addEventListener("load", async () => {
         document.body.removeChild(el);
     });
     results.addEventListener("click", async () => {
-        let res = await fetch("/results");
-        res = res.split("\n");
+        let res = await fetch("/results").then(res => res.text()).then(txt => txt.split("\n"));
         body.innerHTML = "";
         for (let i = 0, len = res.length; i < len; ++i)
             body.innerHTML += decodeAnswers(data, res[i]);
