ae50e62346dd2e8e7b03c174af9f1d9e9590ab4e
Hugo <warion.hugo@gmail.com>
2018-11-23 06:21:50 +0100

    pile réponse

--- web/js/script.js
+++ web/js/script.js
@@ -126,7 +126,7 @@ function next_qu() {
         //console.log("curry_stack.pop() " + tmp);
         let same = false;
         //console.log("[id_to, id_qu, id_sub_qu] " + [id_to, id_qu, id_sub_qu]);
-            while(tmp !== undefined || same){
+            while(tmp !== undefined && same){
                 console.log("while " + (tmp[0] !== id_to || tmp[1] !== id_qu || tmp[2] !== id_sub_qu).toString());
                 if(tmp[0] !== id_to || tmp[1] !== id_qu || tmp[2] !== id_sub_qu){
                     same = true;
